bcff7e321207bf373e596acd61b32163ac586035
Zach Slavin <slavin.zach@gmail.com>
2017-01-07 10:40:02 -0800

    adds stuff

--- handler.py
+++ handler.py
@@ -6,6 +6,7 @@ import hashlib
 def _hmac_is_valid(body, secret, hmac_to_verify):
     hash = hmac.new(secret, body, hashlib.sha1)
     hmac_calculated = base64.b64encode(hash.digest())
+    print(dir(hash))
     print(body)
     print(hash)
     print(hmac_calculated)
